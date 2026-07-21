c6b08181cebd13ca7bf5a82d7afb58e54c8edd4e
MarcosD <marcos.d.lima17@aluno.senai.br>
2026-07-21 15:59:34 -0300

    Refatorar a classe Pontuacao para pontuação de jogos de memória

--- Pontuacao.cs
+++ Pontuacao.cs
@@ -1,48 +1,5 @@
 using System;
 
-<<<<<<< HEAD
-using System;
-
-class SistemaPontuacao
-{
-// Pontuação máxima permitida no jogo.
-private const int PONTUACAO_MAXIMA = 10;
-
-// Variável responsável por armazenar a pontuação atual.
-private int pontuacao = 0;
-
-// Exibe a pontuação atual na tela.
-public void MostrarPontuacao()
-{
-Console.WriteLine($"Pontuação: {pontuacao:D2}");
-}
-
-// Adiciona um ponto ao jogador.
-// O sistema impede que a pontuação ultrapasse 10.
-public void AdicionarPonto()
-{
-if (pontuacao < PONTUACAO_MAXIMA)
-{
-pontuacao++;
-}
-}
-
-// Remove um ponto do jogador.
-// Impede que a pontuação fique negativa.
-public void RemoverPonto()
-{
-if (pontuacao > 0)
-{
-pontuacao--;
-}
-}
-
-// Retorna a pontuação atual.
-public int ObterPontuacao()
-{
-return pontuacao;
-}
-=======
 namespace JogoDaMemoria
 {
     public class Pontuacao
@@ -92,5 +49,4 @@ namespace JogoDaMemoria
             return Math.Max(pontuacaoFinal, 10);
         }
     }
->>>>>>> f6185b0c3edf81d9a3816dfd755db8f7f76f143f
 }
