4f20ebccacf6e227220b5f2b975a81b2e958cd81
MarlonRonie <supergameslindo971@gmail.com>
2026-07-21 15:21:23 -0300

    Incialização do jogo

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-
+namespace JogoDaMemoria
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {  
+            
+            Jogo jogo = new Jogo();
+            jogo.Iniciar();
+        }
+    }
+}
